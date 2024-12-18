76af190e06e6d96dcbb93e29dc7b2908ea4ad9f1
vidhyamanickaraj <vidhyamanickaraj@vidhyas-air.home>
2024-12-17 22:03:53 -0600

    Initial commit in framework

---
+++ Team01_WebScrapers_Dec2024/src/test/java/com/baseclass/BaseTest.java
@@ -0,0 +1,55 @@
+package com.baseclass;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+import com.utilities.PropertyFileReader;
+
+public class BaseTest {
+
+	static String url;
+	public static WebDriver driver;
+	String title;
+
+	public WebDriver setDriver(String browser) {
+		if (browser.equalsIgnoreCase("chrome")) {
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--remote-allow-origins=*");
+			driver = new ChromeDriver();
+		} else if (browser.equalsIgnoreCase("edge"))
+			driver = new EdgeDriver();
+		else if (browser.equalsIgnoreCase("fireFox"))
+			driver = new FirefoxDriver();
+		if (browser.equalsIgnoreCase("safari"))
+			driver = new SafariDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().window().maximize();
+		return driver;
+	}
+
+	public static WebDriver getdriver() {
+		return driver;
+	}
+
+	public static WebDriver getUrl(String key) throws Throwable {
+		url = PropertyFileReader.getGlobalValue(key);
+		driver.get(url);
+		return driver;
+	}
+
+	public static boolean verifyTitle(String title) {
+		if (driver.getTitle().equals(title)) {
+			System.out.println("Page title: " + title + " matched successfully");
+			return true;
+		} else {
+			System.out.println("Page url: " + title + " not matched");
+		}
+		return false;
+	}
+}
---
+++ Team01_WebScrapers_Dec2024/src/test/java/com/utilities/ExcelReader.java
@@ -0,0 +1,45 @@
+package com.utilities;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.Row;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelReader {
+
+	public static String[][] getData(String sheetname, String path) throws IOException {
+
+		FileInputStream FIS = new FileInputStream(new File(path));
+		XSSFWorkbook workbook = new XSSFWorkbook(FIS);
+		XSSFSheet sheet = workbook.getSheet(sheetname);
+		int row = sheet.getLastRowNum();
+		Row rowcell = sheet.getRow(0);
+		int totcol = rowcell.getLastCellNum();
+		DataFormatter format = new DataFormatter();
+		String testdata[][] = new String[row][totcol];
+		for (int i = 1; i <= row; i++) {
+			for (int j = 0; j < totcol; j++) {
+				testdata[i - 1][j] = format.formatCellValue(sheet.getRow(i).getCell(j));
+			}
+		}
+
+		return testdata;
+	}
+
+	public static int getLastColumn(String sheetname, String path) throws IOException {
+
+		FileInputStream FIS = new FileInputStream(new File(path));
+		XSSFWorkbook workbook = new XSSFWorkbook(FIS);
+		XSSFSheet sheet = workbook.getSheet(sheetname);
+		int row = sheet.getLastRowNum();
+		Row rowcell = sheet.getRow(row);
+		int totcol = rowcell.getLastCellNum();
+		return totcol + 1;
+	}
+
+}
